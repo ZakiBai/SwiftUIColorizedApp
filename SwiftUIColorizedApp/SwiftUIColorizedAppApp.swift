adce7da911c491dcaa64f6adb1245d42ed22baac
Zaki <menza.zm@gmail.com>
2023-06-17 13:21:16 +0600

    Initial Commit

---
+++ SwiftUIColorizedApp/SwiftUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedAppApp.swift
+//  SwiftUIColorizedApp
+//
+//  Created by Zaki on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
